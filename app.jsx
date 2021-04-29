7c2c7faacec72763a935da93d1ef9ccdf6d64559
11551378694 <82794852+11551378694@users.noreply.github.com>
2021-04-30 01:22:19 +0800

    update

--- app.jsx
+++ app.jsx
@@ -1,7 +1,12 @@
 const {BrowserRouter, Link, Route, Switch} = ReactRouterDOM;//can be found in external library: ReactDOM
 const Router = BrowserRouter;
 const{useRouteMatch, useParams, useLocation} = ReactRouterDOM;
-const{TransitionGroup, CSSTransition} = ReactTransitionGroup;
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
+import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
+
+mapboxgl.workerClass = MapboxWorker;
+mapboxgl.accessToken = 'pk.eyJ1IjoiMTE1NTEzODY5NCIsImEiOiJja25zajBvNGIwNmNhMnlzMXd0bjIzdnlnIn0.Ap3eCLrjBNsVTQrGNo3tMg';
+
 
 //LongLink is a function, will return ">" when the page is "activated"
 //the useRouteMatch is to check whether the current URL is matched with the "to"
@@ -27,3 +32,43 @@ function NoMatch(){
       </div>
     );
 }
+
+class App extends React.Component{
+    render(){
+        return(
+            <Router>
+                <div>
+                <ul>
+                    <LongLink activeOnlyWhenExact={true} to = "/" label="Home"/> 
+                    <LongLink to = "/mapBox" label="mapBox" />
+                </ul>
+                <hr/>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/mapBox" component={mapBox}/>
+                    <Route path="*" componenet={NoMatch} />
+                </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
+
+class Home extends React.Component {
+    render() {
+      return <h2>Home</h2>;
+    }
+  }
+
+class Map extends React.Component{
+    render(){
+        return(
+            <div className="map">
+                document.write("Hello!")
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Map />, document.querySelector('map'));
+// {ReactDOM.render(<App/>, document.querySelector("#map"));}
