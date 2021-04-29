f6520614705b12ba7dc1de39b30016e406a5ab4d
11551378694 <82794852+11551378694@users.noreply.github.com>
2021-04-29 18:13:38 +0800

    script

---
+++ script.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiMTE1NTEzODY5NCIsImEiOiJja25zajBvNGIwNmNhMnlzMXd0bjIzdnlnIn0.Ap3eCLrjBNsVTQrGNo3tMg';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
