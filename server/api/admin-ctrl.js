b971abe13433d3ea22e1c9079900a2bcb8ed4345
dao-olau <48786829+dao-olau@users.noreply.github.com>
2021-04-30 21:56:49 +0800

    Add update and delete user function

--- server/api/admin-ctrl.js
+++ server/api/admin-ctrl.js
@@ -2,37 +2,86 @@ const User = require('../models/model')
 
 createUser = (req, res) => {
 
-    var account = new User({
-        username: req.body['username'],
-        password: req.body['password'],
-        favouritePlace: []
-    })
+  var account = new User({
+    username: req.body['username'],
+    password: req.body['password'],
+    favouritePlace: []
+  })
 
-    account.save(function(err) {
-        if (err)
-            res.send(err);
+  account.save(function (err) {
+    if (err)
+      res.send(err);
 
-        console.log("Create user success")
-    })
+    console.log("Create user success")
+  })
 }
 
 retrieveUser = (req, res) => {
-    var query = User.findOne({userId: req.params['userId']}, function (err, result) {
-        if (result == null) {
-          return res.send("No existed userID");
-        } else {
-          query.exec(function (err, user) {
-            if (err) return handleError(err);
-              res.send("This is user "+user.userId+":<br>\n" +
-                "Username: " + user.username + "<br>\n" +
-                "Password: " + user.password + "<br>\n");
-            }
-          )}
-      })
+  var query = User.findOne({ userId: req.params['userId'] }, function (err, result) {
+    if (result == null) {
+      return res.send("No existed userID");
+    } else {
+      query.exec(function (err, user) {
+        if (err) return handleError(err);
+        res.send("This is user " + user.userId + ":<br>\n" +
+          "Username: " + user.username + "<br>\n" +
+          "Password: " + user.password + "<br>\n");
+      }
+      )
+    }
+  })
 }
 
+updateUser = (req, res) => {
+  User.findOne({ userId: req.params['userId'] }, (err, result) => {
+    if (result == null) {
+      return res.send("No existed userID");
+    } else {
+      let userObj = {}
+      let flag = 0
+      if (req.body['username'] !== null && req.body['username'] !== undefined) {
+        user["username"] = req.body['username']
+        flag++
+      }
+      if (req.body['password'] !== null && req.body['password'] !== undefined) {
+        user["password"] = req.body['password']
+        flag++
+      }
+      if (flag !== 0) {
+        User.findOneAndUpdate(
+          { userId: req.params['userId'] },
+          userObj,
+          (err, result) => {
+            if (err) return handleError(err);
+            res.send("Update user success");
+          }
+        )
+      } else {
+        res.send("Update Error: no password and no username received");
+      }
+    }
+  })
+}
+
+deleteUser = (req, res) => {
+  User.findOne({ userId: req.params['userId'] }, (err, result) => {
+    if (result == null) {
+      return res.send("No existed userID");
+    } else {
+      User.findOneAndDelete(
+        { userId: req.params['userId'] },
+        (err) => {
+          if (err) return handleError(err);
+          res.send("delete user success");
+        }
+      )
+    }
+  })
+}
 
 module.exports = {
-    createUser,
-    retrieveUser
+  createUser,
+  retrieveUser,
+  updateUser,
+  deleteUser,
 }
--- server/api/admin-router.js
+++ server/api/admin-router.js
@@ -4,5 +4,7 @@ const router = express.Router()
 
 router.post('/admin/create', AdminCtrl.createUser)
 router.get('/admin/user/:userId', AdminCtrl.retrieveUser)
+router.post('/admin/update/:userId', AdminCtrl.updateUser)
+router.post('/admin/delete/:userId', AdminCtrl.deleteUser)
 
 module.exports = router
