1f3c3272e640173654c5d12380b6dc652ba7a49d
dao-olau <48786829+dao-olau@users.noreply.github.com>
2021-05-03 21:53:36 +0800

    Add pages for location CRUD

---
+++ client/src/components/admin-location-update.component.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import UserService from "../services/user.service";
+
+export default class AdminLocationUpdate extends Component {
+    render() {
+        return (
+            <div className="container">
+                <form action="http://csci2720-g23.cse.cuhk.edu.hk/admin/location/update" method="post">
+                    <label for="locationId">locationId: </label>
+                    <input name='locationId' />
+                    <br />
+                    <label for="latitude">latitude: </label>
+                    <input name='latitude' />
+                    <br />
+                    <label for="longitude">longitude: </label>
+                    <input name='longitude' />
+                    <br />
+                    <label for="name">name: </label>
+                    <input name='name' />
+                    <br />
+                    <input type="submit" />
+                </form>
+            </div>
+        );
+    }
+}
