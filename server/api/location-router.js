c67d498ff5d36a32eb98b9bd17829e3580969813
mcchow8 <49152991+mcchow8@users.noreply.github.com>
2021-05-03 15:51:03 +0800

    Update admin and location router

--- server/api/location-router.js
+++ server/api/location-router.js
@@ -2,9 +2,18 @@ const express = require('express')
 const LocationCtrl = require('./location-ctrl')
 const router = express.Router()
 
-router.post('/admin/location/create', LocationCtrl.createLocation)
-router.get('/admin/location/:locationId', LocationCtrl.retrieveLocation)
-router.post('/admin/location/update/:locationId', LocationCtrl.updateLocation)
-router.post('/admin/location/delete/:locationId', LocationCtrl.deleteLocation)
+module.exports = function(app) {
+    app.use(function(req, res, next) {
+        res.header(
+          "Access-Control-Allow-Headers",
+          "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
 
-module.exports = router
+    app.post('/admin/location/create', LocationCtrl.createLocation)
+    app.get('/admin/location/:locationId', LocationCtrl.retrieveLocation)
+    app.post('/admin/location/update/:locationId', LocationCtrl.updateLocation)
+    app.post('/admin/location/delete/:locationId', LocationCtrl.deleteLocation)
+
+}
