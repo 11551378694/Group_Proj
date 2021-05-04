0f7db2c442679676580fb3673fc7964505ea18c4
dao-olau <48786829+dao-olau@users.noreply.github.com>
2021-05-05 00:34:07 +0800

    Remove unused import

--- client/src/components/admin-location-delete.component.js
+++ client/src/components/admin-location-delete.component.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import UserService from "../services/user.service";
 
 export default class AdminLocationDelete extends Component {
     render() {
--- client/src/components/admin-user-retrieve.component.js
+++ client/src/components/admin-user-retrieve.component.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import UserService from "../services/user.service";
+
 
 export default class AdminUserRetrieve extends Component {
     render() {
